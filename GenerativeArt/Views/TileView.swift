0f018fe86706fbd019a3d53e861a580fc2f414e6
Russell Gordon <rgordon@lcs.on.ca>
2023-10-24 12:18:01 -0400

    Got remaining shapes drawn.

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -28,8 +28,49 @@ struct TriangleBottomLeft: Shape {
 }
 
 // TriangleTopLeft
+struct TriangleTopLeft: Shape {
+    
+    // On iOS, the co-ordinate system is flipped
+    // Origin at upper left and positive values on vertical axis extend downward
+    func path(in rect: CGRect) -> Path {
+        
+        // Make an empty path
+        var path = Path()
+        
+        // Draw the path
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))   // 1
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))   // 2
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))   // 3
+        
+        // Return the path
+        return path
+    }
+    
+}
 
 // TriangleBottomRight
+struct TriangleBottomRight: Shape {
+    
+    // On iOS, the co-ordinate system is flipped
+    // Origin at upper left and positive values on vertical axis extend downward
+    func path(in rect: CGRect) -> Path {
+        
+        // Make an empty path
+        var path = Path()
+        
+        // Draw the path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))      // 1
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))   // 2
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))   // 3
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))   // 4
+        
+        // Return the path
+        return path
+    }
+    
+}
 
 // TriangleTopRight
 struct TriangleTopRight: Shape {
@@ -58,18 +99,31 @@ struct TileView: View {
         VStack {
             // Top right triangle
             TriangleTopRight()
+                .stroke(.black)
+                .fill(.clear)
                 // Make the shape be a square
                 .aspectRatio(1.0, contentMode: .fit)
             
             // Bottom left triangle
             TriangleBottomLeft()
+                .stroke(.black)
+                .fill(.clear)
+                // Make the shape be a square
+                .aspectRatio(1.0, contentMode: .fit)
+            
+            // Top left right
+            TriangleTopLeft()
+                .stroke(.black)
+                .fill(.clear)
                 // Make the shape be a square
                 .aspectRatio(1.0, contentMode: .fit)
 
-            
-            // Top left right
-            
             // Bottom right triangle
+            TriangleBottomRight()
+                .stroke(.black)
+                .fill(.clear)
+                // Make the shape be a square
+                .aspectRatio(1.0, contentMode: .fit)
         }
         .padding()
     }
