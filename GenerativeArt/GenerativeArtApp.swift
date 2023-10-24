098dd4f9633c4ddb37b953da5f6efe8b5be5014b
Russell Gordon <rgordon@lcs.on.ca>
2023-10-24 11:30:46 -0400

    Initial Commit

---
+++ GenerativeArt/GenerativeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeArtApp.swift
+//  GenerativeArt
+//
+//  Created by Russell Gordon on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
