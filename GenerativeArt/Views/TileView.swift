b3418efec1b5793c4d7802db2d0a30c5d37f034e
Russell Gordon <rgordon@lcs.on.ca>
2023-10-24 12:27:04 -0400

    Created a random number to simulate a coin and draw the form.

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -8,35 +8,47 @@
 import SwiftUI
 
 struct TileView: View {
+    
+    // MARK: Stored properties
+    let flipOne = Int.random(in: 1...2)
+    
+    // MARK: Computed properties
     var body: some View {
-        VStack {
-            // Top right triangle
-            TriangleTopRight()
-                .stroke(.black)
-                .fill(.clear)
-                // Make the shape be a square
-                .aspectRatio(1.0, contentMode: .fit)
-            
-            // Bottom left triangle
-            TriangleBottomLeft()
-                .stroke(.black)
-                .fill(.clear)
-                // Make the shape be a square
-                .aspectRatio(1.0, contentMode: .fit)
-            
-            // Top left right
-            TriangleTopLeft()
-                .stroke(.black)
-                .fill(.clear)
-                // Make the shape be a square
-                .aspectRatio(1.0, contentMode: .fit)
+        ZStack {
+        
+            if flipOne == 1 {
+                // Top right triangle
+                TriangleTopRight()
+                    .stroke(.black)
+                    .fill(.clear)
+                    // Make the shape be a square
+                    .aspectRatio(1.0, contentMode: .fit)
+                
+                // Bottom left triangle
+                TriangleBottomLeft()
+                    .stroke(.black)
+                    .fill(.clear)
+                    // Make the shape be a square
+                    .aspectRatio(1.0, contentMode: .fit)
 
-            // Bottom right triangle
-            TriangleBottomRight()
-                .stroke(.black)
-                .fill(.clear)
-                // Make the shape be a square
-                .aspectRatio(1.0, contentMode: .fit)
+            } else {
+             
+                // Top left right
+                TriangleTopLeft()
+                    .stroke(.black)
+                    .fill(.clear)
+                    // Make the shape be a square
+                    .aspectRatio(1.0, contentMode: .fit)
+
+                // Bottom right triangle
+                TriangleBottomRight()
+                    .stroke(.black)
+                    .fill(.clear)
+                    // Make the shape be a square
+                    .aspectRatio(1.0, contentMode: .fit)
+                
+            }
+
         }
         .padding()
     }
