7c0388696388171c21a3ba62d6090e7d71ce81a4
Russell Gordon <rgordon@lcs.on.ca>
2023-10-28 10:06:56 -0400

    Replaced three literal TileView instances with three TileView instances created by a loop.

--- GenerativeArt/Views/MosaicView.swift
+++ GenerativeArt/Views/MosaicView.swift
@@ -11,9 +11,19 @@ struct MosaicView: View {
     var body: some View {
         Grid(horizontalSpacing: 0, verticalSpacing: 0) {
             GridRow {
-                TileView()
-                TileView()
-                TileView()
+                ForEach(1..<4) { i in
+                    TileView()
+                }
+            }
+            GridRow {
+                ForEach(1..<4) { i in
+                    TileView()
+                }
+            }
+            GridRow {
+                ForEach(1..<4) { i in
+                    TileView()
+                }
             }
         }
     }
